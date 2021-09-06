2cdfb7306b5b779666454df61d9fa8567bae8aab
Zibilyonik <onatsurmeli@gmail.com>
2021-09-06 14:07:46 +0200

    setup linters, setup api caller, setup store

---
+++ src/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const MONSTER_URL = 'https://www.dnd5eapi.co/api/monsters';
+
+const getMonsters = async (...challengeRating) => {
+  const monsters = [];
+  const link = (challengeRating.length === 0) ? `${MONSTER_URL}?challenge_rating=${challengeRating.join(',')}` : MONSTER_URL;
+  axios.get(link).then((result) => {
+    if (result.status === 200) {
+      const { data } = result;
+      data.forEach((item) => {
+        const monster = {
+          id: item.index,
+          name: item.name,
+          size: item.size,
+          type: item.type,
+          alignment: item.alignment,
+        };
+        monsters.push(monster);
+      });
+    }
+    return monsters;
+  });
+};
+
+export default getMonsters;
